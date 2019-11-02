bb80434a0d9780fb88292de4e3423ec032b923aa
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-02 17:24:48 -0300

    Aula sobre template literals (concatenação)

--- ecmascript6/bundle.js
+++ ecmascript6/bundle.js
@@ -1,58 +1,6 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+var nome = "Lucas";
+var idade = 20; // console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-// REST
-// funciona para objetos
-var usuario = {
-  nome: "Lucas",
-  idade: 20,
-  empresa: "SoftDevLucas"
-};
-
-var nome = usuario.nome,
-    resto = _objectWithoutProperties(usuario, ["nome"]);
-
-console.log(nome);
-console.log(resto); // para vetores
-
-var arr = [1, 2, 3, 4, 5];
-var a = arr[0],
-    b = arr[1],
-    c = arr.slice(2);
-console.log(a, b, c); // e para funções
-
-function soma(a, b) {
-  for (var _len = arguments.length, params = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
-    params[_key - 2] = arguments[_key];
-  }
-
-  // return params.reduce((total,next) => total + next);
-  return params;
-}
-
-console.log(soma(1, 3, 4, 5, 6, 7)); // SPREAD
-
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6];
-var arr3 = [].concat(arr1, arr2);
-console.log(arr3);
-var usuario1 = {
-  nome: "Lucas",
-  idade: 20,
-  empresa: "SoftDevLucas"
-};
-
-var usuario2 = _objectSpread({}, usuario1, {
-  nome: "Gabriel"
-});
-
-console.log(usuario2);
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- ecmascript6/main.js
+++ ecmascript6/main.js
@@ -1,47 +1,5 @@
-// REST
+const nome = "Lucas";
+const idade = 20;
 
-// funciona para objetos
-const usuario = {
-    nome:"Lucas",
-    idade: 20,
-    empresa: "SoftDevLucas"
-}
-
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-// para vetores
-const arr = [1, 2, 3, 4, 5];
-
-const [a, b, ...c] = arr;
-
-console.log(a, b, c);
-
-// e para funções
-
-function soma ( a, b,...params){
-    // return params.reduce((total,next) => total + next);
-    return params;
-}
-
-console.log(soma(1, 3, 4, 5, 6, 7));
-
-// SPREAD
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-console.log(arr3);
-
-const usuario1 = {
-    nome: "Lucas",
-    idade: 20,
-    empresa: "SoftDevLucas",
-};
-
-const usuario2 = {...usuario1, nome: "Gabriel"};
-
-console.log(usuario2);
+// console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
