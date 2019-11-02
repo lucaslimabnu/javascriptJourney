e8649e51ae34c51566100538035b2bb3d4c6ee5d
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-02 17:22:47 -0300

    Aula sobre Rest e Spread no javascript.

--- ecmascript6/main.js
+++ ecmascript6/main.js
@@ -1,28 +1,47 @@
+// REST
+
+// funciona para objetos
 const usuario = {
-    nome: 'Lucas',
-    idade : 20,
-    endereco : {
-        cidade: 'Blumenau',
-        estado: 'SC',
-    },
-};
-
-console.log(usuario);
-
-// const nome = usuario.nome;
-// const idade = usuario.idade;
-// const cidade = usuario.endereco.cidade;
-
-//agora, utilizando desestruturação
-const { nome, idade, endereco : {cidade}} = usuario;
-
-console.log(nome);
-console.log(idade);
-console.log(cidade);
-
-
-function mostraNome( { nome, idade } ){
-    console.log(nome, idade);
+    nome:"Lucas",
+    idade: 20,
+    empresa: "SoftDevLucas"
 }
 
-mostraNome(usuario);
+const { nome, ...resto } = usuario;
+
+console.log(nome);
+console.log(resto);
+
+// para vetores
+const arr = [1, 2, 3, 4, 5];
+
+const [a, b, ...c] = arr;
+
+console.log(a, b, c);
+
+// e para funções
+
+function soma ( a, b,...params){
+    // return params.reduce((total,next) => total + next);
+    return params;
+}
+
+console.log(soma(1, 3, 4, 5, 6, 7));
+
+// SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+const usuario1 = {
+    nome: "Lucas",
+    idade: 20,
+    empresa: "SoftDevLucas",
+};
+
+const usuario2 = {...usuario1, nome: "Gabriel"};
+
+console.log(usuario2);
