ea2e76e00a4c44a80dbd375be2f9a62bb0f85e1d
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-03 14:35:38 -0300

    Exercício realizado.

--- ecmascript6/src/main.js
+++ ecmascript6/src/main.js
@@ -13,3 +13,31 @@ class Api {
 
 Api.getUserInfo('lucaslimabnu');
 Api.getUserInfo('"');
+
+// Funão delay aciona o .then após 1s
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+function umPorSegundo() {
+    delay().then(() => {
+        console.log('1s');
+        delay().then(() => {
+            console.log('2s');
+            delay().then(() => {
+                console.log('3s');
+            });
+        })
+    });
+}
+umPorSegundo();
+
+const doisPorSegundo = async  () => {
+    await delay();
+    console.log("1s d");
+    console.log("1s d");
+    await delay();
+    console.log("2s d");
+    console.log("2s d");
+    await delay();
+    console.log("3s d");
+    console.log("3s d");
+}
+doisPorSegundo();
