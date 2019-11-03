1e78f148dc666875401fcc3204ca7a4e93bcff9b
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-03 14:20:14 -0300

    Aula sobre async e await. Vale muito ver de novo.

--- ecmascript6/src/main.js
+++ ecmascript6/src/main.js
@@ -1,1 +1,30 @@
-alert('Testando');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("OK") }, 2000);
+});
+
+// minhaPromise()
+//     .then( response => {
+//         console.log(response);
+//     })
+//     .catch( err => {
+
+//     });
+
+//sempre deve ser envolto por uma função
+// async function executaPromise() {
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise()); 
+// }
+
+// não se pode utilizar await fora de uma função async
+// await executaPromise();
+// executaPromise();
+
+// functiona com arrowFunction também
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+executaPromise();
