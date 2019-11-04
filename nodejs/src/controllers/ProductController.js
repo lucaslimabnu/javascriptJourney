2ff5ae94a3329a1bbe1e2d2b6c1db72597bdaf39
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-04 09:14:56 -0300

    Realizado o CRUD do produto

--- nodejs/src/controllers/ProductController.js
+++ nodejs/src/controllers/ProductController.js
@@ -4,14 +4,37 @@ const Product = mongoose.model('Product');
 
 module.exports = {
   async index(req, res) {
-    const products = await Product.find(); // o where pode ir aqui
+    // o where pode ir aqui no find
+    const products = await Product.find(); 
 
     return res.json(products);
   },
 
-  async store(req, res) {
-    const product = await Product.create(req.body);
+  async show (req, res) {
+    const product = await Product.findById(req.params.id);
 
     return res.json(product);
-  }
+  },
+
+  async store(req, res) {
+    const product = await Product.create(req.body);
+    
+    return res.json(product);
+  },
+  
+  async update(req, res) {
+    // caso eu queira salvar em product o ANTIGO, não usar o new: true
+    // caso eu queira salvar em product o ATUALIZADO, usar o new: true
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+    return res.json(product);
+    
+  },
+  async destroy(req, res) {
+    const product = await Product.findByIdAndRemove(req.params.id);
+
+    // retorna sucesso sem conteúdo
+    return res.send();
+
+  },
 }
