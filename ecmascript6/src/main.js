c429fc00ca3cedfcc48ff5fea7b0a79bb9f0d6d5
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-03 21:42:00 -0300

    Fazendo as requisições na api do github

--- ecmascript6/src/main.js
+++ ecmascript6/src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class App {
   constructor() {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.inputEl = document.querySelector('input[name=repository]');
     this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
@@ -12,15 +15,29 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event); 
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
 
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0){
+      return;
+    }
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    console.log(response);
+
+    const { name, description, html_url, owner: { avatar_url } } = response.data;
+
     this.repositories.push({
-      name: 'Rocketseat',
-      description: 'Tire a sua ideia do papel e dê vida à sua startup.',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+      name,
+      description,
+      avatar_url,
+      html_url,
     });
+    
+    this.inputEl.value = '';
 
     this.render();
   }
