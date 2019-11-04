92160fe66c9c689948f19fcdf5fa543e9cb5cd07
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-03 21:47:38 -0300

    Finalizando com um elemento de carregamento ao buscar

--- ecmascript6/src/main.js
+++ ecmascript6/src/main.js
@@ -12,7 +12,19 @@ class App {
   }
 
   registerHandlers() {
-    this.formEl.onsubmit = event => this.addRepository(event); 
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  setLoading(loading = true){
+    if(loading === true){
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
   }
 
   async addRepository(event) {
@@ -20,43 +32,52 @@ class App {
 
     const repoInput = this.inputEl.value;
 
-    if (repoInput.length === 0){
+    if (repoInput.length === 0) {
       return;
     }
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    console.log(response);
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    const { name, description, html_url, owner: { avatar_url } } = response.data;
+      console.log(response);
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-    });
-    
+      const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
+
+      
+      this.render();
+    } catch (err) {
+      alert('O repositório não existe');
+    }
+
+    this.setLoading(false);
     this.inputEl.value = '';
 
-    this.render();
   }
 
-  render () {
+  render() {
     this.listEl.innerHTML = '';
     // this.repositories.map() É UTILIZADO QUANDO QUEREMOS ALTERAR ALGO
     // this.repositories.forEach()  É UTILIZADO QUANDO QUEREMOS PERCORRER FAZENDO ALGO
 
-    this.repositories.forEach( repo => {
+    this.repositories.forEach(repo => {
       let imgEl = document.createElement('img');
       imgEl.setAttribute('src', repo.avatar_url);
-      
+
       let titleEl = document.createElement('strong');
       titleEl.appendChild(document.createTextNode(repo.name));
-      
+
       let descriptionEl = document.createElement('p');
       descriptionEl.appendChild(document.createTextNode(repo.description));
-      
+
       let linkEl = document.createElement('a');
       linkEl.setAttribute('target', '_blank');
       linkEl.setAttribute('href', repo.html_url);
