f54d0bfc37b144870ccd55884c6caaa01c0c78c3
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-03 22:37:17 -0300

    Modelo de produto criado

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,31 @@
 const express = require('express'); // retorna uma função
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// iniciando o app
 const app = express();
+
+// iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 
 // primeira rota 
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  Product.create({
+    title: "React native",
+    description: 'build native apps with react',
+    url: 'http://github.com/facebook/react-native'
+  });
+  
+  return res.send("Hello World");
 });
 
 app.listen(3001);
