aabb9414640c985cd481df32a4b1e8a38a9ccd30
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-04 10:48:58 -0300

    Criando e estilizando o componente Header

---
+++ reactjs/huntweb/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.css";
+
+// class Header extends Component {
+//   render() {
+//     return <h1>Hello</h1>
+//   }
+// }
+
+// mesma coisa que 
+
+const Header = () => (<header id="main-header">JSHunt</header>);
+
+export default Header;
