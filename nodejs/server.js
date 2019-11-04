2cb06f7f44e77e3237bc3eef6c3627a4abca1094
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-04 09:58:30 -0300

    Instalando o CORS no projeto

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,14 @@
 const express = require('express'); // retorna uma função
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // iniciando o app
 const app = express();
 app.use(express.json());
+
+// pode ser configurado com direcionamento de domínios externos.
+app.use(cors());
 
 // iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi',
