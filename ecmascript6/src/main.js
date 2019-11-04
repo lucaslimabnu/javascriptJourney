056aeca408c3a2542b387465cdde93859b7b8de7
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-03 21:32:39 -0300

    Fazendo o método de render na tela

--- ecmascript6/src/main.js
+++ ecmascript6/src/main.js
@@ -3,6 +3,7 @@ class App {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
   }
@@ -13,13 +14,46 @@ class App {
 
   addRepository(event) {
     event.preventDefault();
+
     this.repositories.push({
       name: 'Rocketseat',
       description: 'Tire a sua ideia do papel e dê vida à sua startup.',
       avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
       html_url: 'http://github.com/rocketseat/rocketseat.com.br',
     });
-    console.log(this.repositories);
+
+    this.render();
+  }
+
+  render () {
+    this.listEl.innerHTML = '';
+    // this.repositories.map() É UTILIZADO QUANDO QUEREMOS ALTERAR ALGO
+    // this.repositories.forEach()  É UTILIZADO QUANDO QUEREMOS PERCORRER FAZENDO ALGO
+
+    this.repositories.forEach( repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+      
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+      
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+      
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+
+    });
   }
 }
 
