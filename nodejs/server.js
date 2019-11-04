e81dcee60ec9a7d881355dd0dd82270d04080ce5
Lucas Lima <lucaslima.contato00@gmail.com>
2019-11-03 22:01:43 -0300

    Criando a primeira rota do projeto

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,4 +2,9 @@ const express = require('express'); // retorna uma função
 
 const app = express();
 
+// primeira rota 
+app.get('/', (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(3001);
